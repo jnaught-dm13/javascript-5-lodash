21d033a2c60bcdca56e23b97de265284e4cb3d01
Aaron.Nacua <alnacua@gmail.com>
2018-04-10 14:20:53 -0500

    completed all steps

--- lodash.js
+++ lodash.js
@@ -282,11 +282,11 @@ const myEmployees = [
   }
 ];
 
-const myEmployeesAddresses = 0; // use map to get addresses here.
+const myEmployeesAddresses = _.map(myEmployees, "address"); // use map to get addresses here.
 
 // Now we want to use map to get an array of ages of the employees.
 
-const myEmployeesAges = 0; // use map to get ages here.
+const myEmployeesAges = _.map(myEmployees, "age"); // use map to get ages here.
 
 // union
 //  Union lets us take 2 arrays, and create a new array that only has 1 entry for
@@ -326,7 +326,7 @@ const breeFriendsEmails = [
   "dave@gmail.com"
 ];
 
-const listToSendEmailsTo = 0; // Put lodash here to make list of the union of the two address.
+const listToSendEmailsTo = _.union(bobFriendsEmails, breeFriendsEmails); // Put lodash here to make list of the union of the two address.
 
 // intersection
 //  Intersection lets us take two arrays, and create a new array that only
@@ -339,7 +339,7 @@ const listToSendEmailsTo = 0; // Put lodash here to make list of the union of th
 // Bob and Bree have decided that instead of inviting everyone they know to their
 // party. They are instead only going to invite those people that they both know.
 
-const listOfSharedEmails = 0; // Use lodash to create the list that are in both lists
+const listOfSharedEmails = _.intersection(bobFriendsEmails, breeFriendsEmails); // Use lodash to create the list that are in both lists
 
 // groupBy
 //  Group By lets us take an array of objects, and group then into groups based
@@ -403,7 +403,7 @@ const purchases = [
   { month: "February", price: 23.65 }
 ];
 
-const purchasesByMonth = 0; // Use groupBy to group the purchases by the month that they were made.
+const purchasesByMonth = _.groupBy(purchases, "month"); // Use groupBy to group the purchases by the month that they were made.
 
 // Bonus Points
 const totalByMonth = 0; // Use the grouped purchasesByMonth and reduce to create a totalByMonth object.
@@ -440,19 +440,21 @@ function slowFibonnaci(n) {
 //When ready to test uncomment the 3 lines below.
 
 let slowN = 30;
-// console.time('slowFibonnaci:' + slowN)
+// console.time("slowFibonnaci:" + slowN);
 // console.log(slowFibonnaci(slowN));
-// console.timeEnd('slowFibonnaci:' + slowN);
+// console.timeEnd("slowFibonnaci:" + slowN);
 
 let fastN = 1000;
 
-let fastFibonnaci = 0; // use memoize to create a fast fibonnaci.  Use the same
+let fastFibonnaci = _.memoize(function(n) {
+  return n < 2 ? n : fastFibonnaci(n - 1) + fastFibonnaci(n - 2);
+}); // use memoize to create a fast fibonnaci.  Use the same
 // recursve structure that the slowFibonnaci is using, but have it be memoized
 // so that it'll remeber the previous times it's been called and increase the
 
-// console.time('fastFibonnaci:' + fastN)
-// console.log(fastFibonnaci(fastN));
-// console.timeEnd('fastFibonnaci:' + fastN)
+console.time("fastFibonnaci:" + fastN);
+console.log(fastFibonnaci(fastN));
+console.timeEnd("fastFibonnaci:" + fastN);
 
 // We can also use memoize on axios calls so that we only need to make the
 // request to the server once.
@@ -465,37 +467,39 @@ let getDeathstar = function(n) {
 
 // Below we can measure the time it takes to get a return from the api call.
 
-// console.time('getDeathstar')
-// getDeathstar(9).then(e=>{
-//   console.log(e.data)
-//   console.timeEnd('getDeathstar')
-// });
+console.time("getDeathstar");
+getDeathstar(9).then(e => {
+  console.log(e.data);
+  console.timeEnd("getDeathstar");
+});
 
 // getPersonApi `https://swapi.co/api/people/${n}`
 
-let getJedi = // Use Memoize to remember the previous calls made to the server
-  // then compare the times for the first and second calls of both the getJedi and
-  // getDeathstar functions
-  // There are no unit tests for this section. But play around with the
-  // console.time, and console.timeEnd functions so you can use them to measure
-  // the time it takes for various parts of your code to run.  This can be
-  // helpful in finding slow parts of your code that you want to improve.
+let getJedi = _.memoize(function(n) {
+  return axios.get("https://swapi.co/api/starships/" + n);
+}); // Use Memoize to remember the previous calls made to the server
+// then compare the times for the first and second calls of both the getJedi and
+// getDeathstar functions
+// There are no unit tests for this section. But play around with the
+// console.time, and console.timeEnd functions so you can use them to measure
+// the time it takes for various parts of your code to run.  This can be
+// helpful in finding slow parts of your code that you want to improve.
 
-  // console.time('getJedi')
-  // getJedi(1).then(e=>{
-  //   console.log(e.data)
-  //   console.timeEnd('getJedi')
-  // });
+console.time("getJedi");
+getJedi(2).then(e => {
+  console.log(e.data);
+  console.timeEnd("getJedi");
+});
 
-  setTimeout(() => {
-    // console.time('getDeathstar')
-    // getDeathstar(9).then(e=>{
-    //   console.log(e.data)
-    //   console.timeEnd('getDeathstar')
-    // });
-    // console.time('getJedi')
-    // getJedi(1).then(e=>{
-    //   console.log(e.data)
-    //   console.timeEnd('getJedi')
-    // });
-  }, 2000);
+// setTimeout(() => {
+//   console.time("getDeathstar");
+//   getDeathstar(9).then(e => {
+//     console.log(e.data);
+//     console.timeEnd("getDeathstar");
+//   });
+//   console.time("getJedi");
+//   getJedi(1).then(e => {
+//     console.log(e.data);
+//     console.timeEnd("getJedi");
+//   });
+// }, 2000);
